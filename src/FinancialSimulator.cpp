fb8c91037a210f03091b9f802120f23532b64acf
Gunhee LEE <gunheecs.lee@samsung.com>
2021-04-08 13:45:14 +0900

    Added some skeletons.

---
+++ src/FinancialSimulator.cpp
@@ -0,0 +1,5 @@
+#include <nlohmann/json>
+
+using json = nlohmann::json;
+
+
---
+++ src/classes.h
@@ -0,0 +1,33 @@
+/* This file is temporary class files. After some development, each class will be 
+   moved to its own header file.
+*/
+
+class State
+{
+
+};
+
+class Time
+{
+
+};
+
+class Action
+{
+
+};
+
+class WaitAction: public Action
+{
+
+};
+
+class Environment
+{
+
+};
+
+class Asset
+{
+
+};

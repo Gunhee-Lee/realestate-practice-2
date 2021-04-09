f33ff35f9e2a85b04105a110ff890464d7d69119
Gunhee LEE <gunheecs.lee@samsung.com>
2021-04-09 15:11:58 +0900

    Added some class drafts.

--- src/FinancialSimulator.cpp
+++ src/FinancialSimulator.cpp
@@ -1,5 +1,7 @@
 #include <nlohmann/json>
+#include "classes.h"
 
 using json = nlohmann::json;
+
 
 
--- src/classes.h
+++ src/classes.h
@@ -2,19 +2,27 @@
    moved to its own header file.
 */
 
-class State
+class IPrintable
 {
-
+    public:
+    virtual void print() = 0;
 };
 
-class Time
+class State: public virtual IPrintable
 {
-
+    DateTime today;     // Use QuantLib
+    std::vector<Asset> currentAssets_;  // Assets include cash and debts.
 };
 
 class Action
 {
 
+};
+
+class SimulationRun
+{
+    State initialState_;
+    std::vector<Action> actions_;
 };
 
 class WaitAction: public Action
@@ -24,10 +32,30 @@ class WaitAction: public Action
 
 class Environment
 {
+};
+
+// This class will be implemented as a json file.
+class Asset: public virtual IPrintable
+{
+    std::u16string name;
+    Currency getPrice(Time t);  // QuantLib should replace this line soon.
+};
+
+class Person: public virtual IPrintable
+{
+    State currentState_;
 
 };
 
-class Asset
+// Control class.
+class FinancialSimulator
 {
+    private:
+    State initialState_;
+    State desiredState_;
+    Environment env_;
+    SimulationRun run_(initialState);
 
+    public:
+    void makeSimulationRun();
 };
